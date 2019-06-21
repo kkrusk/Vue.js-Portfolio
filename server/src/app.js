925cbbc4de7410248f2ca11c6463c8af46b35818
kkrusk <kylekrusk@gmail.com>
2019-06-21 18:08:36 -0400

    Cleaning stuff up

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,28 @@
 /*eslint-disable */
-console.log("hello");
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
 
-const app = express();
-app.use(morgan('combine'));
-app.use(bodyParser.json());
-app.use(cors());
+const app = express()
 
-app.listen(process.env.PORT || 8081);
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+	res.send('This is confirmation we are connected');
+	console.log('This is confirmation we are connected');
+});
+
+app.post('/register', (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email}! Your user has been created!`,
+    })
+})
+
+app.listen(8081, () => {
+	console.log('User server listening on port 8081');
+});;
 
 /*eslint-enable */
