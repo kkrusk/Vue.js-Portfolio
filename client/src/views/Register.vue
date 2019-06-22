d74ec2b06d4675f7c111680da06a19c63d528c15
kkrusk <kylekrusk@gmail.com>
2019-06-21 21:40:15 -0400

    Receiving response from vue -> node

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,10 +1,45 @@
 <template>
   <div>
     <h1>Register</h1>
+    <input type="email" name="email" placeholder="email" v-model="email">
+    <br>
+    <input type="password" name="password" placeholder="password" v-model="password">
+    <br>
+    <button @click="register">Register</button>
   </div>
 </template>
 
 <script>
+import AuthenticationService from "../services/AuthenticationService.js";
+
+export default {
+  data() {
+    message: "Register";
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  // watch: {
+  //   email(value) {
+  //     console.log("Email has changed", value);
+  //   }
+  // },
+  methods: {
+    async register() {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      });
+      console.log(response.data);
+    }
+  }
+  // mounted() {
+  //   setTimeout(() => {
+  //     this.email = "hello world";
+  //   }, 1000);
+  // }
+};
 </script>
 
 <style>
